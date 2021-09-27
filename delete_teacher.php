12650690e9a09d15a53054f24f9ee0e02612197c
sadmaaan <sadm6n@gmail.com>
2021-09-27 21:58:20 +0600

    features implemented which delete entries from table and also from database

---
+++ delete_teacher.php
@@ -0,0 +1,12 @@
+<?php
+require_once('nav.php');
+require_once('connection.php');
+require_once('create_teacher.php');
+require_once('delete_teacher.php');
+
+
+if (isset($_GET['Delete'])) {
+    $del_id = $_GET['Delete'];
+    $del_sql = "DELETE FROM teacher WHERE teacher_id='$del_id'";
+    $result = mysqli_query($con, $del_sql);
+}
