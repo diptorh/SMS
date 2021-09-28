228515d2bb39f1a7c9127e0d30e1c8c7e8023b8c
Dipto Rahman <dipto.rh@gmail.com>
2021-09-28 21:02:06 +0600

    implemented ajax in create

--- create.php
+++ create.php
@@ -1,10 +1,10 @@
 <?php
-require_once('nav.php');
+//require_once('nav.php');
 require_once('connection.php');
-require_once('delete.php');
+//require_once('delete.php');
 
 
-if (isset($_POST['submit'])) {
+//if (isset($_POST['submit'])) {
     //echo ' Submit button okay. ';
     $stu_name = $_POST['students_name'];
     $fath_name = $_POST['fathers_name'];
@@ -17,4 +17,44 @@ if (isset($_POST['submit'])) {
     $sql = "INSERT INTO student_reg (name, fathers_name, mothers_name,class,birth_date)
     VALUES ('$stu_name', '$fath_name', '$moth_name','$class','$dob')";
     $result = mysqli_query($con, $sql);
+
+
+
+$sql2 = "select * from student_reg  order by id desc ";
+$sql_read = mysqli_query($con, $sql2);
+
+while ($res = mysqli_fetch_assoc($sql_read)) {
+
+                        echo "<tr>
+                            <td>".$res['id']."</td>
+                            <td>".$res['name']."</td>
+                            <td>".$res['fathers_name']."</td>
+                            <td>".$res['mothers_name']." </td>
+                            <td>".$res['birth_date']."</td>
+                            <td>".$res['class']."</td>
+        <td><a class='btn btn-info' type='submit'
+                href='registration.php?Edit=" . $res['id'] . "'>Edit</a>
+            <a class='btn btn-danger' type='submit'
+                href='delete.php?Delete=" . $res['id'] . "'>Delete</a>
+        </td>
+
+    </tr>";
 }
+
+//
+//
+//    "</td>
+//                            <td>"
+//                                .if ($update != true) { ?><!--."-->
+<!--                                <a class='btn btn-info' type='submit'-->
+<!--                                   href='registration.php?Edit=".$res['id']."'>Edit</a>-->
+<!--                                <a class='btn btn-danger' type='submit'-->
+<!--                                   href='save.php?Delete=". $res['id']."'>Delete</a>-->
+<!--                            </td>";-->
+<!--                            --><?php //} ?>
+<!--                        </tr>-->
+<!---->
+<!--                    --><?php //} ?>
+
+
+//}
