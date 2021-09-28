ae7e278dd1c4374f132b39f380a24ee554f254fb
Dipto Rahman <dipto.rh@gmail.com>
2021-09-28 17:15:13 +0600

    ajax implemented on index page

---
+++ index_ajax.php
@@ -0,0 +1,134 @@
+<?php
+require_once('nav.php');
+require_once('connection.php');
+require_once('edit.php');
+
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4  ">
+            <div class="card" style="background-color: #7600a9;">
+                <div class="card-title py-3  text-white text-center">
+                    <h3> Student's Registration </h3>
+                </div>
+
+                <div class="card-body">
+
+                    <form action="#" method="post">
+
+                        <div class="form-group">
+                            <input type="text" class=" form-control" id="students_name" name='students_name'
+                                placeholder="Students Name" value="<?php echo $e_name; ?>" required>
+
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" class=" form-control" id="fathers_name" name='fathers_name'
+                                placeholder="Fathers Name" value="<?php echo $e_fath; ?>">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" class=" form-control" id="mothers_name" name='mothers_name'
+                                placeholder="Mothers Name" value="<?php echo $e_moth; ?>">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="date" class=" form-control" id="dob" name='dob' placeholder="Date of Birth"
+                                value="<?php echo $e_dob; ?>" required>
+                        </div>
+
+                        <div class="form-group">
+                            <select id="class" name="class" class=" form-control " value="<?php echo $e_cla; ?>"
+                                required>
+                                <option value="">--Choose Class --</option>
+                                <option value="1">Class 1</option>
+                                <option value="2">Class 2</option>
+                                <option value="3">Class 3</option>
+                                <option value="4">Class 4</option>
+                                <option value="5">Class 5</option>
+                                <option value="6">Class 6</option>
+                                <option value="7">Class 7</option>
+                                <option value="8">Class 8</option>
+                                <option value="9">Class 9</option>
+                                <option value="10">Class 10</option>
+
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="hidden" class=" form-control" name='update_id' value="<?php echo $edit_id; ?>">
+                        </div>
+
+                        <div class="form-group">
+                            <?php if ($update == true) { ?>
+                            <button class="btn btn-warning" name="update" type="submit">Update</button>
+                            <?php  } else { ?>
+                            <button class="btn btn-success" id="create" name="submit" type="submit">Submit</button>
+                            <?php  }  ?>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-lg-8">
+            <?php require_once('student_info_table.php');
+            ?>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+
+<script>
+$(document).ready(function() {
+
+    $('#create').click(function(e) {
+        e.preventDefault();
+        //var fieldval = '';
+        var students_name1 = $('#students_name').val();
+        var fathers_name1 = $('#fathers_name').val();
+        var mothers_name1 = $('#fathers_name').val();
+        var students_dob1 = $('#teacher_dob').val();
+        var students_class1 = $('#teacher_gender').val();
+
+        //return false;
+        // if (fieldval != '') {
+        $.ajax({
+            url: 'create.php',
+            method: 'POST',
+            data: {
+                students_name: students_name1,
+                fathers_name: fathers_name1,
+                mothers_name: mothers_name1,
+                dob: students_dob1,
+                class: students_class1,
+
+            },
+            success: function(data) {
+                alert("Data inserted Successfully");
+                // $('tbody').html(data);
+
+
+            }
+
+        });
+        // }
+
+    });
+
+
+
+
+
+});
+</script>
+
+
+</html>
